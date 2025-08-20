c94d34c8f736294e664b8d4ca492f0384a65a41a
Shyam <gshyamprasad2001@gmail.com>
2025-08-20 08:32:13 +0530

    feat:added more feautures

--- source.config.ts
+++ source.config.ts
@@ -3,7 +3,11 @@ import {
   defineDocs,
   frontmatterSchema,
   metaSchema,
-} from 'fumadocs-mdx/config';
+} from "fumadocs-mdx/config";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import { transformerTwoslash } from "fumadocs-twoslash";
+import { rehypeCodeDefaultOptions } from "fumadocs-core/mdx-plugins";
 
 // You can customise Zod schemas for frontmatter and `meta.json` here
 // see https://fumadocs.vercel.app/docs/mdx/collections#define-docs
@@ -18,6 +22,21 @@ export const docs = defineDocs({
 
 export default defineConfig({
   mdxOptions: {
-    // MDX options
+
+    rehypePlugins: (v) => [rehypeKatex, ...v],
+    remarkPlugins: [remarkMath],
+    rehypeCodeOptions: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+      transformers: [
+        ...(rehypeCodeDefaultOptions.transformers ?? []),
+        transformerTwoslash(),
+      ],
+    },
+
+    // Place it at first, it should be executed before the syntax highlighter
+    
   },
 });
